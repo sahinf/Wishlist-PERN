8bd5dd501ecfbb759a287440975b10735b49c460
Furkan Sahin <furkansahin@tamu.edu>
2022-04-22 21:42:29 -0500

    cleanup

--- backend/app.js
+++ backend/app.js
@@ -1,38 +1,28 @@
 const http = require('http');
 const express = require('express');
+const app = express();
 
 
 const hostname = '127.0.0.1'
 const port = 8080
 
-//* Routing
-//* basically does "app.use('API_CALL')"
-
-var dogs = require('./routes/dogs');
-const app = express();
-
-app.use('/', (req, res) => {
+//* Landing page: can load an html here
+app.get('/', (req, res) => {
   res.send("Homepage!");
 });
-
-app.use('/dogs', dogs);
 
 //* IDK why
 app.use(express.json())
 
-
-
+//* app.use() on all routes
 const mountRoutes = require('./routes');
 mountRoutes(app);
 
 
 //****** DELETE THIS MAYBE, OR PUT IT IN ITS OWN MODULE N EXPORT IT*/
 
-//** Manual query call using pool */
-const pool = require('./db');
-const { user } = require('pg/lib/defaults');
-const { resolveNaptr } = require('dns');
-// const { prototype } = require('aws-sdk/clients/apigateway');
+//** Manual query call using Pool */
+const manual_pool = require('./db');
 
 //* https://node-postgres.com/features/queries
 const query = {
@@ -41,14 +31,13 @@ const query = {
   values: [1],
 }
 //* This method is not an API call to a query, but rather the query call itself
-//* I instead want to make API query calls
-//* Daisy-Chaining promises!
-// pool.query(query)
-//   .then((res) => console.table(res.rows))
-//   .catch( (e) => console.log(e));
+//* I instead want to make API query calls - Daisy-Chaining promises!
+manual_pool.query(query)
+  .then((res) => console.table(res.rows))
+  .catch( (e) => console.log(e));
 
 //* testing a function I am exporting from backend/db/index.js
-pool.testing()
+manual_pool.testing()
 
 //* TRY APIs
 // console.log('now testing express route');
@@ -59,14 +48,14 @@ pool.testing()
 
 
 
-//* This is useful if 'pool' referred to a Pool object I made,
-//* however 'pool' refers to the object we imported from /backend/db/index.js
-// pool.connect()
+//* This is useful if 'manual_pool' referred to a manual_pool object I made,
+//* however 'manual_pool' refers to the object we imported from /backend/db/index.js
+// manual_pool.connect()
 // .then(() => console.log('Connected to database successfully'))
-// .then(() => pool.query('SELECT * FROM users')) // this returns results
+// .then(() => manual_pool.query('SELECT * FROM users')) // this returns results
 // .then((results) => console.table(results.rows))
 // .catch(e => console.log(e))
-// .finally(() => pool.end())
+// .finally(() => manual_pool.end())
 
 //****** DELETE THIS MAYBE, OR PUT IT IN ITS OWN MODULE N EXPORT IT*/
 
