1a3bfa63f8c5af09fda2a6dafb1778d82d85638a
Furkan Sahin <furkansahin@tamu.edu>
2022-04-22 21:42:12 -0500

    API for querying works perfectly fine!

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -17,6 +17,7 @@ module.exports = router;
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const { rows } = await db.query('SELECT * FROM users where id = $1', [id]);
+  console.log(id);
+  const { rows } = await db.query('SELECT * FROM users where users_id = $1', [id]);
   res.send(rows[0]);
 })
