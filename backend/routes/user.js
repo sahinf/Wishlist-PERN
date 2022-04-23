dd2d918d8a97eca0a37abca17855c1f2539ebbf1
Furkan Sahin <furkansahin@tamu.edu>
2022-04-23 00:12:59 -0500

    put request to update password given a JSON

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,7 @@
 //* How custom APIs can be made with Express routing
 //* https://node-postgres.com/guides/async-express
 
+const { json } = require('express');
 const Router = require('express-promise-router');
 
 const db = require('../db');
@@ -26,6 +27,7 @@ module.exports = router;
  * res.send() vs res.json() : https://stackoverflow.com/questions/19041837/difference-between-res-send-and-res-json-in-express-js
  * essentially, json forces the data into json format and calls res.send()
  * res.send(100) will send a status code, res.json(100) will send a json number
+ * There is no difference if the returned value is always an array/json
  * 
  */
 
@@ -40,4 +42,13 @@ router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const { rows } = await db.query('SELECT * FROM users WHERE users_id = $1', [id]);
   res.send(rows[0]);
+})
+
+//* Update password (or anything) of a user
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  const newPass = body['user_password'];
+  const update = await db.query('UPDATE users SET user_password = $1 WHERE users_id = $2', [newPass, id]);
+  res.json('User was updated');
 })
