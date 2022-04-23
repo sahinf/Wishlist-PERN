73de6fdd1ad95a9ec9c7b9b840531dcd5fc7069e
Furkan Sahin <furkansahin@tamu.edu>
2022-04-22 22:01:45 -0500

    remove daisy chain and manual pool

--- backend/app.js
+++ backend/app.js
@@ -18,34 +18,6 @@ app.use(express.json())
 const mountRoutes = require('./routes');
 mountRoutes(app);
 
-
-//***** Manual query call using Pool */
-const manual_pool = require('./db');
-
-// const query = {
-//   name: 'fetch-user',
-//   text: 'SELECT * FROM users WHERE users_id = $1',
-//   values: [1],
-// }
-// manual_pool.query(query)
-//   .then((res) => console.table(res.rows))
-//   .catch( (e) => console.log(e));
-
-//* testing a function I am exporting from backend/db/index.js
-manual_pool.testing()
-
-//* This is useful if 'manual_pool' referred to a manual_pool object I made,
-//* however 'manual_pool' refers to the object we imported from /backend/db/index.js
-// manual_pool.connect()
-// .then(() => console.log('Connected to database successfully'))
-// .then(() => manual_pool.query('SELECT * FROM users')) // this returns results
-// .then((results) => console.table(results.rows))
-// .catch(e => console.log(e))
-// .finally(() => manual_pool.end())
-
-//****** DELETE THIS MAYBE, OR PUT IT IN ITS OWN MODULE N EXPORT IT*/
-
-
 //**** Create a server object:
 // const server = http.createServer((req, res) => {
 //   res.statusCode = 200;
