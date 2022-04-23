9a47358a7f0204ed08f321b24e959ec12bf15041
Furkan Sahin <furkansahin@tamu.edu>
2022-04-22 22:00:26 -0500

    Cleanup

--- backend/app.js
+++ backend/app.js
@@ -19,34 +19,20 @@ const mountRoutes = require('./routes');
 mountRoutes(app);
 
 
-//****** DELETE THIS MAYBE, OR PUT IT IN ITS OWN MODULE N EXPORT IT*/
-
-//** Manual query call using Pool */
+//***** Manual query call using Pool */
 const manual_pool = require('./db');
 
-//* https://node-postgres.com/features/queries
-const query = {
-  name: 'fetch-user',
-  text: 'SELECT * FROM users WHERE users_id = $1',
-  values: [1],
-}
-//* This method is not an API call to a query, but rather the query call itself
-//* I instead want to make API query calls - Daisy-Chaining promises!
-manual_pool.query(query)
-  .then((res) => console.table(res.rows))
-  .catch( (e) => console.log(e));
+// const query = {
+//   name: 'fetch-user',
+//   text: 'SELECT * FROM users WHERE users_id = $1',
+//   values: [1],
+// }
+// manual_pool.query(query)
+//   .then((res) => console.table(res.rows))
+//   .catch( (e) => console.log(e));
 
 //* testing a function I am exporting from backend/db/index.js
 manual_pool.testing()
-
-//* TRY APIs
-// console.log('now testing express route');
-// const axios = require('axios');
-// axios.get(`http://localhost:${port}/users/34`)
-// .then( (res) => console.log(res))
-// .catch( (e) => console.log(e))
-
-
 
 //* This is useful if 'manual_pool' referred to a manual_pool object I made,
 //* however 'manual_pool' refers to the object we imported from /backend/db/index.js
