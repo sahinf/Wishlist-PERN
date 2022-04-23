765765cc6aaa11a59a56b92b3a99ce737b84c19d
Furkan Sahin <furkansahin@tamu.edu>
2022-04-22 20:43:51 -0500

    correctly creating server based on express(), allowing API calls to get handled

--- backend/app.js
+++ backend/app.js
@@ -6,15 +6,33 @@ const hostname = '127.0.0.1'
 const port = 8080
 
 //* Routing
-const mountRoutes = require('./routes');
+//* basically does "app.use('API_CALL')"
 
+var dogs = require('./routes/dogs');
 const app = express();
+
+app.use('/', (req, res) => {
+  res.send("Homepage!");
+});
+
+app.use('/dogs', dogs);
+
+//* IDK why
+app.use(express.json())
+
+
+
+const mountRoutes = require('./routes');
 mountRoutes(app);
 
 
 //****** DELETE THIS MAYBE, OR PUT IT IN ITS OWN MODULE N EXPORT IT*/
 
+//** Manual query call using pool */
 const pool = require('./db');
+const { user } = require('pg/lib/defaults');
+const { resolveNaptr } = require('dns');
+// const { prototype } = require('aws-sdk/clients/apigateway');
 
 //* https://node-postgres.com/features/queries
 const query = {
@@ -22,9 +40,15 @@ const query = {
   text: 'SELECT * FROM users WHERE users_id = $1',
   values: [1],
 }
+//* This method is not an API call to a query, but rather the query call itself
+//* I instead want to make API query calls
+//* Daisy-Chaining promises!
+// pool.query(query)
+//   .then((res) => console.table(res.rows))
+//   .catch( (e) => console.log(e));
 
-pool.query(query)
-.then( (res) => console.table(res.rows))
+//* testing a function I am exporting from backend/db/index.js
+pool.testing()
 
 //* TRY APIs
 // console.log('now testing express route');
@@ -35,7 +59,8 @@ pool.query(query)
 
 
 
-// app.use()
+//* This is useful if 'pool' referred to a Pool object I made,
+//* however 'pool' refers to the object we imported from /backend/db/index.js
 // pool.connect()
 // .then(() => console.log('Connected to database successfully'))
 // .then(() => pool.query('SELECT * FROM users')) // this returns results
@@ -46,23 +71,18 @@ pool.query(query)
 //****** DELETE THIS MAYBE, OR PUT IT IN ITS OWN MODULE N EXPORT IT*/
 
 
-// Create a server object:
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('Howdy\n');
-  res.end('Hello, slut!\n');
-})
+//**** Create a server object:
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.write('res.write message: Howdy\n');
+//   res.end('res.end message: Ending message!\n');
+// })
 
-// Set up our server so it will listen on the port
-server.listen(port, hostname, (error) => {
-
-  // Checking any error occur while listening on port
-  if (error) {
-    console.log('Something went wrong', error);
-  }
-  // Else sent message of listening
-  else {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  }
-})
+//* Default page
+app.use('*', (req, res) => {
+  res.sendStatus('404').send('Page not found');
+});
+http.createServer(app).listen(port, hostname, () =>{
+  console.log(`Server listening on https://${hostname}:${port}`);
+});
