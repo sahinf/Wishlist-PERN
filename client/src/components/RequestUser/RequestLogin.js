a8a43aaea83b1cbb825a93d35c35377454d12842
Furkan Sahin <furkansahin@tamu.edu>
2022-05-01 15:31:52 -0500

    use await/async with try/catch rather than .then

--- client/src/components/RequestUser/RequestLogin.js
+++ client/src/components/RequestUser/RequestLogin.js
@@ -5,20 +5,22 @@ import { loginURL } from '../../URLs';
  * @param {*} formData data of the form ()
  * @description takes in an object of {users_id, user_password}
  * and sends that as a POST request to login API ("domain/login")
- * @returns JSON Web Token created for user
+ * @returns {string} JSON Web Token created for user
  */
 export const loginRequest = async (formData) => {
-  await axios({
-    url: loginURL(),
-    method: "post",
-    data: formData,
-  })
-    .then((data) => { return data; })
-    .catch((err) => {
-      if (err.response.status === 401 || err.response.status ===500) {
-        alert(err.response.data);
-      }
-      return;
-    })
-    .finally( () => {return;} )
+  try {
+    const { data } = await axios({
+      url: loginURL(),
+      method: "post",
+      data: formData,
+    });
+
+    return data;
+  } catch (e) {
+    console.log(e.response);
+    if (e.response.status === 401 || e.response.status === 500) {
+      alert(e.response.data);
+    }
+    return;
+  }
 };
