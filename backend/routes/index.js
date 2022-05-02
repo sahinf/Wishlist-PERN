1ed3c28208defdf10afeffc8f25e95062b1a99ea
Furkan Sahin <furkansahin@tamu.edu>
2022-05-02 01:01:44 -0500

    wishlist/count API works (tested)

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,13 @@
 const users = require('./user');
 const dogs = require('./dogs');
 const login = require('./login');
+const wishlist = require('./wishlist');
 
 
 //* List all other modules to export as router
 module.exports = app => {
   app.use('/users', users),
-  app.use('/dogs', dogs)
-  app.use('/login', login)
-}
+  app.use('/dogs', dogs),
+  app.use('/login', login),
+  app.use('/wishlist', wishlist)
+};
---
+++ backend/routes/wishlist.js
@@ -0,0 +1,27 @@
+const Router = require('express-promise-router');
+
+const db = require('../db');
+
+const router = new Router();
+
+module.exports = router;
+
+//* Get wishlist count!
+router.post('/count', async (req, res) => {
+  try {
+    const id = req.body['users_id'];
+    if (!id) {
+      return res.status(401).json("wishlist/count: Please provide a user_id");
+    }
+
+    const { rows } = await db.query(
+      'SELECT * FROM wishlist where users_id=$1', [id]
+    );
+
+    res.status(200).json( {count: rows.length});
+
+  } catch (e) {
+    console.error(e.message);
+    res.status(501).json("Sever Error in querying wishlist count");
+  }
+});
