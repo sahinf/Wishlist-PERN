e34227dfb0af1dd003c10cc91e81b7d8cad1d428
Furkan Sahin <furkansahin@tamu.edu>
2022-05-03 03:40:51 -0500

    Employee API DONE

--- backend/routes/employees.js
+++ backend/routes/employees.js
@@ -1,4 +1,5 @@
 const Router = require('express-promise-router');
+const { userIdURL } = require('../../client/src/URLs');
 
 const db = require('../db');
 
@@ -35,13 +36,48 @@ router.put('/', async (req, res) => {
 
     const test = await db.query('SELECT * FROM employee WHERE users_id=$1',[users_id]);
 
-    //* Employee doesn't exist, INSERT it
+    //* INSERT: Employee ID doesn't exist
     if (test.rowCount < 1) {
-      await db.query('INSERT INTO employee ')
+      await db.query('INSERT INTO employee (users_id, employee_fname, employee_lname) VALUES ($1, $2, $3)',[users_id, employee_fname, employee_lname]);
+
+      res.status(200).json(`Employee ${employee_fname} ${employee_lname} inserted into table with id ${users_id}`);
     }
+
+    //* UPDATE: Employee ID exists in table
+    await db.query('UPDATE employee SET users_id=$1, employee_fname=$2, employee_lname=$3',[users_id, employee_fname, employee_lname]);
+
+    res.status(200).json(`Employee updated with id=${users_id}, first name=${employee_fname}, last name=${employee_lname}`);
     
   } catch (e) {
     console.error(e.message);
     res.status(500).json('Server error in adding/updating employee')
   }
 });
+
+//* REMOVE employee
+router.delete('/', async (req, res) => {
+  try {
+    const {users_id} = req.body;
+    if (!users_id) {
+      res.status(401).json(`Server: no input provided`);
+    }
+
+    const test = await db.query('SELECT * FROM employee WHERE users_id=$1', [users_id]);
+    if (test.rowCount < 1) {
+      res.status(401).json(`Server: employee id does not exist`);
+    }
+
+    //* DELETE if not foreign key in use in another table
+    const r = await db.query('DELETE FROM employee WHERE users_id=$1', [users_id]);
+    if (!r) {
+      res.status(400).json(`Cannot delete entities that are existing as foreign keys of another table`)
+    }
+
+    //* DELETE work
+    res.status(200).json(`Employee was deleted`);
+    
+  } catch (e) {
+    console.error('ERROR', e.message);
+    res.status(501).json("Server: Error in deleting employee");
+  }
+})
