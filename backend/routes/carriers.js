d6f013ec51fad429b747ea77518f75a7e74ae936
Furkan Sahin <furkansahin@tamu.edu>
2022-05-03 03:41:07 -0500

    cleanup carrier API

--- backend/routes/carriers.js
+++ backend/routes/carriers.js
@@ -38,11 +38,13 @@ router.put('/', async (req, res) => {
     //* INSERT: Carrier ID doesn't exist
     if (test.rowCount < 1) {
       await db.query('INSERT INTO carrier (carrier_id, carrier_name, carrier_phone) VALUES ($1, $2, $3)', [carrier_id, carrier_name, carrier_phone]);
+
       res.status(200).json(`Carrier ${carrier_name} inserted into table with phone number ${carrier_phone}`)
     }
     
-    //* UPDATE: Carrier NAME exists in table
+    //* UPDATE: Carrier ID exists in table
     await db.query('UPDATE carrier SET carrier_name=$1,carrier_phone=$2 WHERE carrier_id=$3',[ carrier_name, carrier_phone, carrier_id]);
+    
     res.status(200).json(`Carrier ${carrier_name} was updated with phone number ${carrier_phone}`);
 
   } catch (e) {
@@ -71,7 +73,7 @@ router.delete('/', async (req, res) => {
 
     //* Let user know that the query failed because they tried to delete existing foreign key
     if (!r) {
-      res.status(428).json('Cannot delete carriers that are existing foreign keys in other tables')
+      res.status(400).json('Cannot delete carriers that are existing foreign keys in other tables')
     }
 
     res.status(200).json('Carrier was deleted!');
